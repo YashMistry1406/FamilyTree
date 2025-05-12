c2a022e549541247bf1ef56d49a6068612e99a6d
YashMistry1406 <yashmistry2000@gmail.com>
2025-05-12 10:11:35 +0530

    got the nodes and edges to work from backend and frontend

--- src/App.jsx
+++ src/App.jsx
@@ -1,55 +1,45 @@
 import {
-  ReactFlow,
-  Background,
-  Controls,
-  applyEdgeChanges,
-  applyNodeChanges,
-  addEdge,
+    ReactFlow,
+    Background,
+    Controls,
+    applyEdgeChanges,
+    applyNodeChanges,
+    addEdge,
 } from '@xyflow/react';
 import '@xyflow/react/dist/style.css';
 
 import { useCallback, useState, useEffect } from 'react';
 import MenuContext from './MenuContext';
 import CustomNode from './CustomNode';
+import axios from 'axios';
 
-const nodeType  = {
-        customNode : CustomNode
-    }
 const initialNodes = [
-  {
-    id: 'node-1',
-    type: 'customNode',
-    position: { x: 0, y: 0 },
-    data: { value: 123 },
-  },
-  {
-    id: 'node-2',
-    type: 'customNode',
-    position: { x: 100, y: 100 },
-    data: { value: 321},
-  }
 ];
 
+const nodeType = {
+    customNode: CustomNode
+}
 const initialEdges = [];
 
 function App() {
-    
-    const[nodes , setNodes] = useState(initialNodes);
-    const[edges, setEdges] = useState(initialEdges);
+
+
+    const [nodes, setNodes] = useState(initialNodes);
+    const [edges, setEdges] = useState(initialEdges);
     const [contextMenu, setContextMenu] = useState(null); // { x, y, edgeId }
 
     const onNodesChange = useCallback(
-        (changes) => setNodes((nds) => applyNodeChanges(changes,nds)),[]
+        (changes) => setNodes((nds) => applyNodeChanges(changes, nds)), []
     )
 
     const onEdgeChange = useCallback(
-        (changes) => setEdges((edgs) => applyEdgeChanges(changes,edgs)),[]
+        (changes) => setEdges((edgs) => applyEdgeChanges(changes, edgs)), []
     )
 
     const onConnect = useCallback(
-        (conn) => setEdges((edgs)=> addEdge(conn,edgs)),[]
-    )
-
+        (connection) => setEdges((eds) => addEdge(connection, eds)),
+        [setEdges]
+    );
 
     const onEdgeContextMenu = (event, edge) => {
         event.preventDefault();
@@ -62,11 +52,17 @@ function App() {
     const handleOptionClick = (action) => {
         if (action === 'Delete') {
             setEdges((eds) => eds.filter((e) => e.id !== contextMenu.edgeId));
-        } else {
+        }
+        else if (action === 'Edit') {
+            console.log(nodes)
+        }
+        else {
             console.log(`${action} on edge ${contextMenu.edgeId}`);
         }
         setContextMenu(null);
     };
+
+    function randomNumber() { return Math.floor(Math.random() * 100) + 1; }
 
     useEffect(() => {
         const close = () => setContextMenu(null);
@@ -74,33 +70,90 @@ function App() {
         return () => window.removeEventListener('click', close);
     }, []);
 
-    return(
-        <>
-    <div style={{ width: '100vw', height: '100vh' }}>
-        <ReactFlow
-        nodes = {nodes}
-        edges={edges}
-        onNodesChange={onNodesChange}
-        onEdgesChange={onEdgeChange}
-        nodeTypes={nodeType}
-        onConnect={onConnect}
-        onContextMenu={onEdgeContextMenu}
-        >
-        <Background/>
-        <Controls/>
-        </ReactFlow>
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await axios.get('http://localhost:8080/allusers');
 
-            {contextMenu && (
-                <MenuContext
-                    x={contextMenu.x}
-                    y={contextMenu.y}
-                    onOptionClick={handleOptionClick}
-                />
-            )}
-    </div>
+
+                //response.data.forEach(itr=> {
+                //    console.log(itr.relationship.sourceId)
+                //})
+                console.log(response)
+
+                const newNode = response.data.map(item => ({
+                    id: item.from.ID,
+                    type: 'customNode',
+                    position: { x: randomNumber(), y: randomNumber() },
+                    data: { name: item.from.name }
+                }))
+
+                const uniqueNodes = [
+                    ...nodes,
+                    ...newNode.filter(
+                        (newNode) => !nodes.some((existingNode) => existingNode.id === newNode.id)
+                    )
+                ];
+
+
+                const newEdges = response.data
+                    .filter(itr => itr.relationship !== undefined)
+                    .map(itr => ({
+                        id: `e${itr.relationship.sourceId.ID}-${itr.relationship.targetId.ID}`,
+                        source: itr.relationship.sourceId.ID,
+                        target: itr.relationship.targetId.ID
+                    }));
+                setEdges((prevEdges) => {
+                    const existingIds = new Set(prevEdges.map(e => e.id));
+                    const dedupedEdges = newEdges.filter(e => !existingIds.has(e.id));
+                    return [...prevEdges, ...dedupedEdges];
+                });
+
+                //
+                //setEdges((prevEdges) => [
+                //    ...prevEdges,
+                //    ...newEdges
+                //]);
+                setNodes(uniqueNodes)
+            } catch (error) {
+                console.error('Error fetching data:', error);
+            }
+        }
+
+        fetchData();
+    }, [])
+
+    return (
+        <>
+            <div style={{
+                width: '100vw',
+                height: '100vh',
+            }}>
+                <ReactFlow
+                    style={{ backgroundColor: '#D3D3D3' }}
+                    nodes={nodes}
+                    edges={edges}
+                    onNodesChange={onNodesChange}
+                    onEdgesChange={onEdgeChange}
+                    nodeTypes={nodeType}
+                    onConnect={onConnect}
+                    onEdgeContextMenu={onEdgeContextMenu}
+                >
+                    <Background color='#000000' />
+                    <Controls />
+                </ReactFlow>
+
+                {contextMenu && (
+                    <MenuContext
+                        x={contextMenu.x}
+                        y={contextMenu.y}
+                        onOptionClick={handleOptionClick}
+                    />
+                )}
+            </div>
         </>
     )
-    
+
 }
 
 export default App;
--- src/CustomNode.jsx
+++ src/CustomNode.jsx
@@ -2,35 +2,26 @@ import { Handle, Position } from "@xyflow/react";
 import { useCallback } from "react";
 const handleStyle = { left: 10 };
 
-function CustomNode({ data, isConnectable }) {
-  const onChange = useCallback((evt) => {
-    console.log(evt.target.value);
-  }, []);
- 
+function CustomNode({id,type,data,isConnectable}) {
+  //const onChange = useCallback((evt) => {
+  //  console.log(evt.target.value);
+  //}, []);
   return (
-    <div className="text-updater-node">
+      <div className="profile-card">
       <Handle
         type="target"
         position={Position.Top}
         isConnectable={isConnectable}
       />
-      <div>
-        <label htmlFor="text">Text:</label>
-        <input id="text" name="text" onChange={onChange} className="nodrag" />
-      </div>
+      <img className= "profile-pic" src="/src/assets/yash_recent.jpg" alt="Profile" />
+      <h2>{data.name}</h2>
+      <p>Bio</p>
       <Handle
         type="source"
         position={Position.Bottom}
-        id="a"
-        style={handleStyle}
         isConnectable={isConnectable}
       />
-      <Handle
-        type="source"
-        position={Position.Bottom}
-        id="b"
-        isConnectable={isConnectable}
-      />
+
     </div>
   );
 }
